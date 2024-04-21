f4e41d1b78949efc83a13eef1ba6615302b4bd63
David Lee <seekdavidlee@gmail.com>
2024-04-21 16:03:42 -0500

    add LLamaSharp

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-https://github.com/ollama/ollama
+Check out the following:
+
+* https://huggingface.co/easynet/microsoft-phi-2-GGUF/tree/main
+* https://github.com/ollama/ollama
+* https://scisharp.github.io/LLamaSharp/0.11.2/QuickStart/
+* https://github.com/ksanman/ChromaDBSharp
---
+++ chatbot2/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+// C:\Users\seekd\.cache\lm-studio\models\easynet\microsoft-phi-2-GGUF\phi-2.Q6_K.gguf
+using LLama.Common;
+using LLama;
+
+string modelPath = Environment.GetEnvironmentVariable("ModelFilePath") ?? throw new Exception("Missing ModelFilePath!"); // change it to your own model path.
+
+var parameters = new ModelParams(modelPath)
+{
+    ContextSize = 1024, // The longest length of chat as memory.
+    GpuLayerCount = 5 // How many layers to offload to GPU. Please adjust it according to your GPU memory.
+};
+using var model = LLamaWeights.LoadFromFile(parameters);
+using var context = model.CreateContext(parameters);
+var executor = new InteractiveExecutor(context);
+
+// Add chat histories as prompt to tell AI how to act.
+var chatHistory = new ChatHistory();
+chatHistory.AddMessage(AuthorRole.System, "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.");
+chatHistory.AddMessage(AuthorRole.User, "Hello, Bob.");
+chatHistory.AddMessage(AuthorRole.Assistant, "Hello. How may I help you today?");
+
+ChatSession session = new(executor, chatHistory);
+
+InferenceParams inferenceParams = new()
+{
+    MaxTokens = 256, // No more than 256 tokens should appear in answer. Remove it if antiprompt is enough for control.
+    AntiPrompts = new List<string> { "User:" }, // Stop generation once antiprompts appear.
+    Temperature = 0,
+    TopK = 10,
+    TopP = 0.1F
+};
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("The chat session has started.\nUser: ");
+Console.ForegroundColor = ConsoleColor.Green;
+string userInput = Console.ReadLine() ?? "";
+
+while (userInput != "exit")
+{
+    await foreach ( // Generate the response streamingly.
+        var text
+        in session.ChatAsync(
+            new ChatHistory.Message(AuthorRole.User, userInput),
+            inferenceParams))
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(text);
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    userInput = Console.ReadLine() ?? "";
+}
