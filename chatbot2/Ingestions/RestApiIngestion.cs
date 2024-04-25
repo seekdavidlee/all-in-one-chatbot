836b6f724979e3fd3d468d58d5555e80d00c0244
David Lee <leedavid@microsoft.com>
2024-04-25 07:14:00 -0500

    complete rest api ingestion

--- chatbot2/Ingestions/RestApiIngestion.cs
+++ chatbot2/Ingestions/RestApiIngestion.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using chatbot2.Ingestions;
+using Microsoft.Extensions.Logging;
 
 namespace chatbot2.Ingestions;
 
@@ -7,8 +9,10 @@ public class RestApiIngestion : IVectorDbIngestion
     private readonly RestClientConfig config;
     private readonly IRestClientAuthHeaderProvider restClientAuthHeaderProvider;
     private readonly HttpClient httpClient;
+    private readonly ILogger<RestApiIngestion> logger;
     private readonly IEmbedding embedding;
-    public RestApiIngestion(IEnumerable<IEmbedding> embeddings, IRestClientAuthHeaderProvider restClientAuthHeaderProvider, HttpClient httpClient)
+    private readonly int BatchSize = 30;
+    public RestApiIngestion(IEnumerable<IEmbedding> embeddings, IRestClientAuthHeaderProvider restClientAuthHeaderProvider, HttpClient httpClient, ILogger<RestApiIngestion> logger)
     {
         embedding = embeddings.GetSelectedEmbedding();
         var config = JsonSerializer.Deserialize<RestClientConfig>(
@@ -19,9 +23,19 @@ public class RestApiIngestion : IVectorDbIngestion
             throw new Exception("Failed to deserialize RestClientConfig");
         }
 
+        var ingestionBatchSize = Environment.GetEnvironmentVariable("IngestionBatchSize");
+        if (ingestionBatchSize is not null)
+        {
+            if (int.TryParse(ingestionBatchSize, out int batchSize))
+            {
+                BatchSize = batchSize;
+            }
+        }
+
         this.config = config;
         this.restClientAuthHeaderProvider = restClientAuthHeaderProvider;
         this.httpClient = httpClient;
+        this.logger = logger;
         this.httpClient.BaseAddress = new Uri(config.BaseUrl ?? throw new Exception("config url is invalid"));
     }
 
@@ -64,8 +78,9 @@ public class RestApiIngestion : IVectorDbIngestion
                 throw new Exception("Failed to deserialize response content");
             }
 
-            continuationRoute = ((JsonElement)page[config.ContinuationField]).Deserialize<string>();
+            logger.LogInformation("Processing page {continuationRoute}", continuationRoute);
 
+            continuationRoute = page.GetValue(config.ContinuationField)?.Value;
 
             var records = ((JsonElement)page[config.RecordsField]).Deserialize<IDictionary<string, object>[]>();
             if (records is not null)
@@ -76,13 +91,22 @@ public class RestApiIngestion : IVectorDbIngestion
                     bool shouldAdd = true;
                     if (config.AndConditions is not null)
                     {
-
                         foreach (var condition in config.AndConditions)
                         {
                             if (condition.SourceField is not null && condition.Operator == "equal")
                             {
-                                var compareValue = ((JsonElement)record[condition.SourceField]).Deserialize<string>();
+                                var compareValue = record.GetValue(condition.SourceField)?.Value;
                                 if (compareValue == condition.Value)
+                                {
+                                    shouldAdd = false;
+                                    break;
+                                }
+                            }
+
+                            if (condition.SourceField is not null && condition.Operator == "isNotNull")
+                            {
+                                var evalRec = record.GetValue(condition.SourceField);
+                                if (evalRec is null || evalRec.Value.Value is null)
                                 {
                                     shouldAdd = false;
                                     break;
@@ -95,13 +119,29 @@ public class RestApiIngestion : IVectorDbIngestion
                         continue;
                     }
 
-                    searchModels.Add(Create(record, config.Mappings));
+                    searchModels.Add(await CreateAsync(embedding, record, config.Mappings));
+
+                    if (searchModels.Count >= BatchSize)
+                    {
+                        logger.LogInformation("Uploading batch of {0} records", searchModels.Count);
+                        await vectorDb.ProcessAsync(searchModels);
+                        searchModels.Clear();
+                    }
+                }
+
+                if (searchModels.Count > 0)
+                {
+                    logger.LogInformation("Uploading batch of {0} records", searchModels.Count);
+                    await vectorDb.ProcessAsync(searchModels);
+                    searchModels.Clear();
                 }
             }
         }
     }
 
-    private static SearchModel Create(IDictionary<string, object> record, RestClientMapping[] mappings)
+    const string DictionaryToJsonCoversion = "DictionaryToJson";
+
+    private static async Task<SearchModel> CreateAsync(IEmbedding embedding, IDictionary<string, object> record, RestClientMapping[] mappings)
     {
         var searchModel = new SearchModel
         {
@@ -114,16 +154,60 @@ public class RestApiIngestion : IVectorDbIngestion
                 continue;
             }
 
+            var items = mapping.Sources.Select(record.GetValue).Where(x => x is not null && x.Value.Value is not null);
+
+            string mappedValue;
+            if (mapping.Conversion == DictionaryToJsonCoversion)
+            {
+                var dic = new Dictionary<string, string>();
+                foreach (var item in items)
+                {
+                    dic[item?.Key ?? throw new Exception("key is invalid")] = item?.Value ?? throw new Exception("value is invalid");
+                }
+
+                if (mapping.Tags is not null)
+                {
+                    foreach (var tag in mapping.Tags)
+                    {
+                        dic[tag.Key] = tag.Value;
+                    }
+                }
+                mappedValue = JsonSerializer.Serialize(dic);
+            }
+            else
+            {
+                mappedValue = string.Join(mapping.SourcesJoinChar, items.Select(x => x?.Value));
+            }
+
             if (mapping.Target == "id")
             {
-                var ids = mapping.Sources.Select(src => ((JsonElement)record[src]).Deserialize<string>());
-                searchModel.Id = string.Join("_", ids);
+                searchModel.Id = mappedValue;
             }
 
             if (mapping.Target == "title")
             {
-                var titles = mapping.Sources.Select(src => ((JsonElement)record[src]).Deserialize<string>());
-                searchModel.Title = string.Join("\n", titles);
+
+                searchModel.Title = mappedValue;
+            }
+
+            if (mapping.Target == "content")
+            {
+                searchModel.Content = mappedValue;
+            }
+
+            if (mapping.Target == "url")
+            {
+                searchModel.Url = mappedValue;
+            }
+
+            if (mapping.Target == "meta_json_string")
+            {
+                searchModel.MetaData = mappedValue;
+            }
+
+            if (mapping.Target == "contentVector")
+            {
+                searchModel.ContentVector = await embedding.GetEmbeddingsAsync(mappedValue);
             }
         }
         return searchModel;
